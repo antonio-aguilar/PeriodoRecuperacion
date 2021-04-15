4bfd6b9a05ec5606514575e2939c2934cad45291
Antonio Aguilar <55695218+antonio-aguilar@users.noreply.github.com>
2021-04-15 23:05:02 +0200

    Ejercicio RA2-3 Funciones

---
+++ Funciones/Ejer5/FuncionesRA2-3_5.php
@@ -0,0 +1,43 @@
+<?php
+/* Realizar una función que reciba un número y devuelva si es primo o no.*/
+
+echo "<form action=\"FuncionesRA2-3_5.php\" method=\"post\">";
+echo "<p>Numero: <input type=\"number\" name=\"numero\"></p>";
+echo "<p><input type=\"submit\" value=\"Enviar\"></p>";
+echo "</form>";
+
+// Numero a comprobar si es primo o no
+//$numero = 5;
+
+
+function primo($numero)
+{
+    // Total divisores
+    $total = 0;
+
+    for ($i = 1; $i <= $numero; $i++) {
+        if ($numero % $i == 0) {
+            $total += 1;
+        }
+    }
+    // si el total es mayor a 2 entonces no es primo de lo contrario si lo es
+    if ($total > 2) {
+        return false;
+    } else {
+        return true;
+    }
+}
+
+// Mensaje para el usuario
+if (isset($_REQUEST["numero"]) && $_REQUEST["numero"] != "" && $_REQUEST["numero"] != 0) {
+    $numero = $_REQUEST["numero"];
+    if (primo($numero)) {
+        echo "<b>El numero " . $numero . " es primo.</b>";
+    } else {
+        echo "<b>El numero " . $numero . " no es primo.</b>";
+    }
+} else {
+    echo "Inserte un numero";
+}
+
+?>
