2378f0aa12159abbeb095c668426861611dec163
Antonio Aguilar <55695218+antonio-aguilar@users.noreply.github.com>
2021-05-03 23:20:56 +0200

    Ejercicio RA2-3-6 Lista de tareas

---
+++ ListaTareas/lista_tareas.sql
@@ -0,0 +1,66 @@
+-- phpMyAdmin SQL Dump
+-- version 4.6.6deb5ubuntu0.5
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: localhost:3306
+-- Tiempo de generación: 29-04-2021 a las 10:49:43
+-- Versión del servidor: 5.7.33-0ubuntu0.18.04.1
+-- Versión de PHP: 7.2.34-18+ubuntu18.04.1+deb.sury.org+1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `tareas`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `lista_tareas`
+--
+
+CREATE TABLE `lista_tareas` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `descripcion` text NOT NULL,
+  `completada` tinyint(1) NOT NULL DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `lista_tareas`
+--
+
+INSERT INTO `lista_tareas` (`id`, `descripcion`, `completada`) VALUES
+(1, 'tarea de prueba 1', 0),
+(2, 'tarea de prueba 2', 0),
+(3, 'tarea de prueba 3', 0),
+(4, 'tarea de prueba 4', 0);
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `lista_tareas`
+--
+ALTER TABLE `lista_tareas`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `lista_tareas`
+--
+ALTER TABLE `lista_tareas`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
