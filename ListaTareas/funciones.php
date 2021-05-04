f4838f46b3350d6f2614b64f536fd41a12e303c1
Antonio Aguilar <55695218+antonio-aguilar@users.noreply.github.com>
2021-05-04 13:48:32 +0200

    Ejercicio RA2-3-6 Lista de tareas

---
+++ ListaTareas/funciones.php
@@ -0,0 +1,30 @@
+<?php
+
+function encabezado() {
+    ?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Tareas</title>
+</head>
+<body>
+
+<h1>Tareas pendientes de Antonio Aguilar</h1>
+<?php
+}
+
+// Footer de la web
+function pie() {
+    ?>
+<hr />
+<footer> <p>Copyright © Antonio Manuel Aguilar Caro</p> </footer>
+</body>
+</html>
+<?php
+}
+
+?>
