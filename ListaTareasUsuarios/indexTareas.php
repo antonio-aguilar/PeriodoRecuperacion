fab22ff84482bc10a02aa050959ea28dc0572347
Antonio Aguilar <55695218+antonio-aguilar@users.noreply.github.com>
2021-05-20 18:10:13 +0200

    Ejercicio RA2-3-6 Lista de tareas con usuarios

---
+++ ListaTareasUsuarios/indexTareas.php
@@ -0,0 +1,47 @@
+<?php
+session_start();
+/* Desarrolla una página web que muestre una lista de cosas por hacer, más usuarios */
+
+// Llamada a archivos requeridos
+require_once "funciones.php";
+require_once "bd.php";
+require_once "funcionesConsultas.php";
+//require_once "carga.php";
+
+/* Cabecera html de la web */
+encabezado();
+
+// Si se ha enviado el formulario, usar la funcion añadirTarea(bd, descripcion)
+if (isset($_POST["enviar"])) {
+    $descripcion = $_POST["descripcion"];
+    añadirTarea($bd, $descripcion);
+}
+
+/* Contenido de la web */
+if (isset($_SESSION['usuario'])) {
+    
+    mostrarFormularioTareas();
+} else {
+    echo "<p>Logueate para poder añadir tareas!!</p>";
+}
+
+if (isset($_GET["mostrar_completadas"]) == 1) {
+    listarTareas1($bd);
+} else {
+    listarTareas($bd);
+}
+
+if (isset($_SESSION['usuario'])) {
+    // Enlace para mostrar las tareas completadas
+    echo "<br /> <a href=\"indexTareas.php?mostrar_completadas=1\"> Mostrar Completadas </a>";
+}
+
+// Incluir algunos usuarios de prueba con el siguiente archivo php
+//include "carga.php";
+// Listar Usuarios
+listarUsuarios($bd);
+
+/* Pie html de la web */
+pie();
+
+?>
